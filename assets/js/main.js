90b2958b5cae9e546607bd8d5f1ea05523e60efc
Saju Varghese <sajuputhancharuvil@gmail.com>
2026-05-26 11:22:48 +0300

    Remove scroll event listener for navbar background

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,1 @@
-window.addEventListener('scroll', () => {
 
-    const navbar = document.querySelector('.navbar');
-
-    if(window.scrollY > 50) {
-        navbar.style.background = 'rgba(4,8,22,0.95)';
-    }
-    else {
-        navbar.style.background = 'rgba(4,8,22,0.8)';
-    }
-
-});
